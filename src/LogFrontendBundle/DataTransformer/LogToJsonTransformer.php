8b99f330e80aa8009574ebece414d70a143481e4
Tomasz <tarnawski27@gmail.com>
2016-08-11 22:46:28 +0200

    Add date to log

--- src/LogFrontendBundle/DataTransformer/LogToJsonTransformer.php
+++ src/LogFrontendBundle/DataTransformer/LogToJsonTransformer.php
@@ -9,9 +9,10 @@ class LogToJsonTransformer
     public function transform(Log $log)
     {
         $data = [
-            'level' => $log->level ? $log->level : null,
+            'level_name' => $log->level ? $log->level : null,
             'message' => $log->message ? $log->message : null,
-            'context' => $log->context ? $log->context : null
+            'context' => $log->context ? $log->context : null,
+            'datetime' => new \DateTime()
         ];
 
         return json_encode($data);
